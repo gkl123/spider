46fc14b403fe931b726f2db6a78efbea5fdb1978
gkl <gkl@DESKTOP-C0M3Q8G>
2018-12-06 13:59:23 +0800

    新增提交

--- src/main/java/com/jskj/reptile/htmlparser/JsoupUtils.java
+++ src/main/java/com/jskj/reptile/htmlparser/JsoupUtils.java
@@ -1,9 +1,12 @@
 package com.jskj.reptile.htmlparser;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
+
+import com.alibaba.fastjson.JSONObject;
 
 public class JsoupUtils {
 	public static void main(String[] args) {
@@ -15,15 +18,34 @@ public class JsoupUtils {
 					.execute();
 			
 			System.out.println(res.body());
+			
+//			需要用到的参数是admin_id
+			JSONObject jsonRes = JSONObject.parseObject(res.body());
+			JSONObject jsonData = jsonRes.getJSONObject("data");
+			JSONObject jsonUserInfo = jsonData.getJSONObject("user_info");
+			String adminId = jsonUserInfo.getString("admin_id");
+			String token = jsonData.getString("token");
 //			String sessionId = res.cookie("JSESSIONID");
 //			System.out.println(Jsoup.connect("http://prod.admin.timescy.com/api/user/login")
 //			.cookie("JSESSIONID", sessionId).get().body());
+			HttpUtils http = new HttpUtils();
+			HashMap<String, Object> params = new HashMap<String, Object>();
+			params.put("per_page", 15);
+			params.put("page", 1);
+			params.put("admin_id", adminId);
+			params.put("current", 1);
+			HashMap<String, String> headers = new HashMap<String, String>();
+			headers.put("Authorization", "Bearer " + token);
+			headers.put("Content-Type", "application/json,text/plain,*/*;charset=GBK");
+			headers.put("Accept-Language", "zh-CN,zh;q=0.9");
+			String result = http.doGet("http://prod.admin.timescy.com/api/overdue/list", headers, params);
+			System.out.println("result : " + result);
+			
+			// 请求地址 http://prod.admin.timescy.com/api/overdue/list?per_page=15&page=1&admin_id=e6524bf7d829c26cf467076d0729c3f2&time=&paid_status=&current=1
+			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-				
 	}
 
 }
